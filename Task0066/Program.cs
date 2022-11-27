24fbcece727c55498e77e0ed6c7c09353eefb123
DariaBsk <Dariabsk602@gmail.com>
2022-11-27 18:47:55 +0300

    Добавили решение задач

---
+++ Task0066/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+//натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int SumNaturalNumbers(int numМ, int numN)
+{
+    if (numМ <= numN) return numN + SumNaturalNumbers(numМ, numN - 1);
+    else return 0;
+}
+
+Console.Write("Введите натуральное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m > n) (m,n) = (n,m);
+
+int result = SumNaturalNumbers(m, n);
+Console.Write($"{result} ");
---
+++ Task0068/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+
+uint AckermanFunction(uint n, uint m)
+{
+  if (n == 0)
+    return m + 1;
+  else if ((n != 0) && (m == 0))
+    return AckermanFunction(n - 1, 1);
+  else
+    return AckermanFunction(n - 1, AckermanFunction(n, m - 1));
+}
+
+Console.Write("Введите челое положительное число M: ");
+uint m = Convert.ToUInt32(Console.ReadLine());
+
+Console.Write("Введите челое положительное число N: ");
+uint n = Convert.ToUInt32(Console.ReadLine());
+
+uint result = AckermanFunction(m, n);
+Console.Write($"{result} ");
